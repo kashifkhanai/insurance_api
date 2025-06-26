63c1d16ba88b86d623a868bd9a2e8f1d50eb71f9
Kashif Khan <kashifkhan5641502@gmail.com>
2025-06-26 17:27:32 +0500

    Create predict file to save to ml code

---
+++ Model/predict.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+
+
+# Define the version of the model
+model_version = "1.0.0"
+
+# Load the pre-trained model
+with open("Model/model.pkl", "rb") as f:
+    model = pickle.load(f)
+    
+def predict_output(user_input: dict):
+    input_df = pd.DataFrame(user_input)
+    predict_output = model.predict(input_df)[0]
+    return predict_output
+
+
