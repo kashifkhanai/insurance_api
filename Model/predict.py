6c73d27d0ee32d3ab95bac9d252707a2274b2fc6
Kashif Khan <kashifkhan5641502@gmail.com>
2025-06-26 18:02:54 +0500

    Modify Predict file

--- Model/predict.py
+++ Model/predict.py
@@ -10,8 +10,11 @@ with open("Model/model.pkl", "rb") as f:
     model = pickle.load(f)
     
 def predict_output(user_input: dict):
+    
     input_df = pd.DataFrame(user_input)
-    predict_output = model.predict(input_df)[0]
-    return predict_output
+    
+    output = model.predict(input_df)[0]
+    
+    return output
 
 
